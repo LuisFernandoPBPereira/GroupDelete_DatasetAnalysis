bbf0834b0648dc33d3bce86a24f9df53d8ddb276
Luis Fernando <86135150+LuisFernandoPBPereira@users.noreply.github.com>
2025-09-08 21:49:20 -0300

    feat: adicionando bulk insert

---
+++ bulk-insert.sql
@@ -0,0 +1,8 @@
+BULK INSERT vacinacao_jan_2025 -- Caso seja necessário alterar o nome da tabela, primeiro atualize o arquivo CreateGeral e, em seguida, ajuste também neste local.
+FROM 'C:\caminho\arquivo.csv'  -- Caminho do arquivo CSV
+WITH (
+    FIELDTERMINATOR = ';',   -- Separador de campos (padrão: `;`)
+    ROWTERMINATOR = '\n',    -- Separador de linhas
+    FIRSTROW = 2,            -- Ignora a primeira linha (cabeçalho)
+    TABLOCK                  -- Otimiza a inserção em lote
+);
