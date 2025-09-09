d792abd58fb5800b785e9feab5c1555aec076d9a
Ricardo Júnior <56940605+isem49@users.noreply.github.com>
2025-09-09 15:10:51 -0300

    feat: Adicionando INSERT
    - Foi adicionado o INSERT da tabela PaisPaciente

--- SQL/INSERTS.sql
+++ SQL/INSERTS.sql
@@ -1,7 +1,17 @@
--- ObservaÁ„o: O uso do DISTINCT È necess·rio para garantir que n„o sejam inseridos registros duplicados.
--- As colunas est„o mapeadas conforme o dicion·rio de dados (De/Para).
+-- Observa√ß√£o: O uso do DISTINCT √© necess√°rio para garantir que n√£o sejam inseridos registros duplicados, mas nem todas devem ter.
+-- As colunas est√£o mapeadas conforme o dicion√°rio de dados (De/Para).
+
+-- Tabela RacaCorPaciente
 INSERT INTO RacaCorPaciente (CodigoRacaCorPaciente, NomeRacaCorPaciente)
 SELECT DISTINCT 
        co_raca_cor_paciente, 
        no_raca_cor_paciente
 FROM vacinacao_jan_2025; -- Tabela de origem importada via BULK INSERT
+
+-- Tabela PaisPaciente
+INSERT INTO PaisPaciente  (CodigoPaisPaciente , NomePaisPaciente )
+SELECT DISTINCT  
+       co_pais_paciente, 
+       no_pais_paciente
+FROM vacinacao_jan_2025 -- Tabela de origem importada via BULK INSERT
+WHERE vacinacao_jan_2025.co_pais_paciente IS NOT NULL; -- Existe um campo nulo na tabela, ent√£o √© necess√°rio ter este where
