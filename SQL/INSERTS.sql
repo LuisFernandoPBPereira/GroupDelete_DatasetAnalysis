d8f4db4363e45e20a5dffd448c2e4a53292d1f4c
Ricardo Júnior <56940605+isem49@users.noreply.github.com>
2025-09-12 21:49:51 -0300

    Feat: Adicionando CAST nas colunas da tabela EtniaIndigenaPaciente
    - Adicionado CAST na coluna co_etnia_indigena_paciente para converter de NVARCHAR para CHAR(4)
    - Adicionado CAST na coluna no_etnia_indigena_paciente para converter de NVARCHAR para VARCHAR(100)

--- SQL/INSERTS.sql
+++ SQL/INSERTS.sql
@@ -12,16 +12,18 @@ FROM vacinacao_jan_2025;                         -- Tabela de origem importada v
 INSERT INTO PaisPaciente  (CodigoPaisPaciente , NomePaisPaciente )
 SELECT DISTINCT  
        CAST(co_pais_paciente AS INT), -- Conversão de NVARCHAR para INT
-       CAST(no_pais_paciente AS VARCHAR(100)) -- Conversão de NVARCHAR para VARCHAR(50) 
+       CAST(no_pais_paciente AS VARCHAR(100)) -- Conversão de NVARCHAR para VARCHAR(100) 
 FROM vacinacao_jan_2025 -- Tabela de origem importada via BULK INSERT
 WHERE vacinacao_jan_2025.co_pais_paciente IS NOT NULL; -- Existe um campo nulo na tabela, então é necessário ter este where
 
--- Tabela EtniaIndigenaPaciente
+-- Inserindo dados distintos na tabela EtniaIndigenaPaciente
 INSERT INTO EtniaIndigenaPaciente  (CodigoEtniaIndigenaPaciente , NomeEtniaIndigenaPaciente )
 SELECT DISTINCT  
-       co_etnia_indigena_paciente, 
-       no_etnia_indigena_paciente
+       CAST(co_etnia_indigena_paciente AS CHAR(4)), -- Conversão de NVARCHAR para CHAR(4) 
+       CAST(no_etnia_indigena_paciente AS VARCHAR(100)) -- Conversão de NVARCHAR para VARCHAR(100) 
 FROM vacinacao_jan_2025 -- Tabela de origem importada via BULK INSERT
 WHERE vacinacao_jan_2025.co_etnia_indigena_paciente IS NOT NULL; -- Existe um campo nulo na tabela, então é necessário ter este where
+
+
 
 
