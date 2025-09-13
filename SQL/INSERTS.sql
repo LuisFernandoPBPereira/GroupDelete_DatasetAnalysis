9e67133777d2828f3799c97d7a8d90196e04c7da
Ricardo Júnior <56940605+isem49@users.noreply.github.com>
2025-09-12 21:15:01 -0300

    Feat: Adicionando CAST nas colunas da tabela RacaCorPaciente
    - Adicionando CAST na coluna co_raca_cor_paciente para converter de NVARCHAR para INT
    - Adicionando CAST na coluna no_raca_cor_paciente para converter de NVARCHAR para VARCHAR

--- SQL/INSERTS.sql
+++ SQL/INSERTS.sql
@@ -1,12 +1,12 @@
 -- Observação: O uso do DISTINCT é necessário para garantir que não sejam inseridos registros duplicados, mas nem todas devem ter.
 -- As colunas estão mapeadas conforme o dicionário de dados (De/Para).
 
--- Tabela RacaCorPaciente
+-- Inserindo dados distintos na tabela RacaCorPaciente
 INSERT INTO RacaCorPaciente (CodigoRacaCorPaciente, NomeRacaCorPaciente)
 SELECT DISTINCT 
-       co_raca_cor_paciente, 
-       no_raca_cor_paciente
-FROM vacinacao_jan_2025; -- Tabela de origem importada via BULK INSERT
+       CAST(co_raca_cor_paciente AS INT),         -- Conversão de NCHAR para INT 
+       CAST(no_raca_cor_paciente AS VARCHAR(50)) -- Conversão de NVARCHAR para VARCHAR(50) 
+FROM vacinacao_jan_2025;                         -- Tabela de origem importada via BULK INSERT
 
 -- Tabela PaisPaciente
 INSERT INTO PaisPaciente  (CodigoPaisPaciente , NomePaisPaciente )
@@ -23,3 +23,4 @@ SELECT DISTINCT
        no_etnia_indigena_paciente
 FROM vacinacao_jan_2025 -- Tabela de origem importada via BULK INSERT
 WHERE vacinacao_jan_2025.co_etnia_indigena_paciente IS NOT NULL; -- Existe um campo nulo na tabela, então é necessário ter este where
+
