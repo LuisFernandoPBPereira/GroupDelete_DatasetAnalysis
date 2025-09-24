ef9f86d8910383367ecb6d163498bd6068ba9039
Ricardo Júnior <56940605+isem49@users.noreply.github.com>
2025-09-24 13:49:09 -0300

    Refactor: Alterando INSERT de EstrategiaVacinacao
    - Foi adicionado a função TRIM para evitar espaços no começo e no fim.
    - Além disso foi colocada a função MIN para pegar o primeiro valor de uma PK.

--- SQL/INSERTS.sql
+++ SQL/INSERTS.sql
@@ -137,14 +137,40 @@ FROM dbo.vacinacao_jan_2025 AS vj25
 WHERE vj25.co_origem_registro IS NOT NULL AND vj25.ds_origem_registro IS NOT NULL
 
 -- Inserindo dados distintos na tabela EstrategiaVacinacao
+ Inserindo dados distintos na tabela EstrategiaVacinacao
 INSERT INTO EstrategiaVacinacao (CodigoEstrategiaVacinacao, DescricaoEstrategiaVacinacao)
-SELECT DISTINCT
-       CAST(v.co_estrategia_vacinacao AS INT),   -- Conversão de NVARCHAR para INT 
-       CAST(v.ds_estrategia_vacinacao AS VARCHAR(150))       -- Conversão de NVARCHAR para VARCHAR(150)
-FROM vacinacao_jan_2025 v
-WHERE co_estrategia_vacinacao IS NOT NULL;  -- Filtro para evitar campos nulos
+SELECT 
+    CAST(TRIM(v.co_estrategia_vacinacao) AS INT), -- Converte para int
+    CAST(MIN(TRIM(v.ds_estrategia_vacinacao)) AS VARCHAR(100)) -- Converte para varchar, além disso o primeiro valor daquele código
+WHERE v.co_estrategia_vacinacao IS NOT NULL
+  AND v.ds_estrategia_vacinacao IS NOT NULL
+GROUP BY v.co_estrategia_vacinacao;
 
--- FALTANDO DOCUMENTO
+--Inserindo dados distintos na tabela Documento
+INSERT INTO Documento 
+	(
+		CodigoDocumento, 
+		CodigoPaciente, 
+		CodigoTrocaDocumento, 
+		StDocumento, 
+		DataEntradaRnDescricao, 
+		DataDeletadoRnDescricao, 
+		CodigoSistemaOrigem, 
+		CodigoOrigemRegistro, 
+		CodigoEstrategiaVacinacao
+	)
+SELECT DISTINCT
+	CAST(TRIM(co_documento) AS CHAR(41)),
+	CAST(TRIM(co_paciente) AS CHAR(64)),
+	CAST(TRIM(co_troca_documento) AS CHAR(41)),
+	CAST(TRIM(st_documento) AS VARCHAR(20)),
+	CAST(TRIM(dt_entrada_rnds) AS DATETIME2),
+	CAST(TRIM(dt_deletado_rnds) AS DATETIME2),
+	CAST(TRIM(co_sistema_origem) AS INT),
+	CAST(TRIM(co_origem_registro) AS INT),
+	CAST(TRIM(co_estrategia_vacinacao) AS INT)
+FROM vacinacao_jan_2025
+WHERE co_documento IS NOT NULL
 
 -- Inserindo dados distintos na tabela DoseVacina
 INSERT INTO DoseVacina(CodigoDoseVacina,DescricaoDoseVacina)
@@ -205,32 +231,6 @@ SELECT DISTINCT
 	CAST(co_lote_vacina AS VARCHAR(100))   ---- Conversão de NCHAR para VARCHAR(100)
 FROM vacinacao_jan_2025   -- Tabela de origem importada via BULK INSERT
 WHERE co_vacina IS NOT NULL;   -- Filtro para evitar campos nulos
-
---Inserindo dados distintos na tabela Documento
-INSERT INTO Documento 
-	(
-		CodigoDocumento, 
-		CodigoPaciente, 
-		CodigoTrocaDocumento, 
-		StDocumento, 
-		DataEntradaRnDescricao, 
-		DataDeletadoRnDescricao, 
-		CodigoSistemaOrigem, 
-		CodigoOrigemRegistro, 
-		CodigoEstrategiaVacinacao
-	)
-SELECT DISTINCT
-	CAST(TRIM(co_documento) AS CHAR(41)),
-	CAST(TRIM(co_paciente) AS CHAR(64)),
-	CAST(TRIM(co_troca_documento) AS CHAR(41)),
-	CAST(TRIM(st_documento) AS VARCHAR(20)),
-	CAST(TRIM(dt_entrada_rnds) AS DATETIME2),
-	CAST(TRIM(dt_deletado_rnds) AS DATETIME2),
-	CAST(TRIM(co_sistema_origem) AS INT),
-	CAST(TRIM(co_origem_registro) AS INT),
-	CAST(TRIM(co_estrategia_vacinacao) AS INT)
-FROM vacinacao_jan_2025
-WHERE co_documento IS NOT NULL
 
 -- Inserindo dados distintos na tabela AplicacaoVacina
 INSERT INTO AplicacaoVacina (CodigoDocumento, CodigoPaciente, CodigoCnesEstabelecimento, CodigoVacina, DataVacina)
@@ -355,6 +355,7 @@ SELECT DISTINCT
 	CAST(TRIM(co_pais_paciente) AS INT)
 FROM vacinacao_jan_2025
 WHERE co_paciente IS NOT NULL AND co_pais_paciente IS NOT NULL;
+
 
 
 
