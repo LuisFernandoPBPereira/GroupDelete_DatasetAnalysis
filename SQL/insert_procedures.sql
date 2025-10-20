c32d906467f59bd9df1bc9f19910ff9abb71e822
Ricardo Júnior <56940605+isem49@users.noreply.github.com>
2025-10-20 13:34:15 -0300

    Feat: Adicionando verificação de duplicidade nas procedures

--- SQL/insert_procedures.sql
+++ SQL/insert_procedures.sql
@@ -3,13 +3,17 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Inserindo dados distintos na tabela RacaCorPaciente
+    IF EXISTS (SELECT 1 FROM RacaCorPaciente)
+    BEGIN
+        PRINT 'Os dados já foram inseridos anteriormente na tabela RacaCorPaciente.';
+        RETURN;
+    END;
+
     INSERT INTO RacaCorPaciente (CodigoRacaCorPaciente, NomeRacaCorPaciente)
     SELECT DISTINCT 
-           CAST(TRIM(co_raca_cor_paciente) AS INT),         -- Conversão de NCHAR para INT 
-           CAST(TRIM(no_raca_cor_paciente) AS VARCHAR(50)) -- Conversão de NVARCHAR para VARCHAR(50) 
-    FROM vacinacao_jan_2025;                         -- Tabela de origem importada via BULK INSERT
-    -- Não precisa de WHERE pois não tem valores nulos
+           CAST(TRIM(co_raca_cor_paciente) AS INT),
+           CAST(TRIM(no_raca_cor_paciente) AS VARCHAR(50))
+    FROM vacinacao_jan_2025;
 
     PRINT 'Dados inseridos com sucesso na tabela RacaCorPaciente.';
 END;
@@ -22,16 +26,22 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Inserindo dados distintos na tabela PaisPaciente
-    INSERT INTO PaisPaciente  (CodigoPaisPaciente , NomePaisPaciente )
+    IF EXISTS (SELECT 1 FROM PaisPaciente)
+    BEGIN
+        PRINT 'Os dados já foram inseridos anteriormente na tabela PaisPaciente.';
+        RETURN;
+    END;
+
+    INSERT INTO PaisPaciente (CodigoPaisPaciente, NomePaisPaciente)
     SELECT DISTINCT  
-           CAST(TRIM(co_pais_paciente) AS INT), -- Conversão de NVARCHAR para INT
-           CAST(TRIM(no_pais_paciente) AS VARCHAR(100)) -- Conversão de NVARCHAR para VARCHAR(100) 
-    FROM vacinacao_jan_2025 -- Tabela de origem importada via BULK INSERT
-    WHERE vacinacao_jan_2025.co_pais_paciente IS NOT NULL; -- Existe um campo nulo na tabela, então é necessário ter este where
+           CAST(TRIM(co_pais_paciente) AS INT),
+           CAST(TRIM(no_pais_paciente) AS VARCHAR(100))
+    FROM vacinacao_jan_2025
+    WHERE co_pais_paciente IS NOT NULL;
 
     PRINT 'Dados inseridos com sucesso na tabela PaisPaciente.';
 END;
 GO
 
 EXEC InserirPaisPaciente;
+
