4f0112e2a5052d61dcb069ce0f1e2a7d241af575
Ricardo Júnior <56940605+isem49@users.noreply.github.com>
2025-10-20 13:24:14 -0300

    Refactor: Renomeando arquivos de Procedure
    Colocando o nome no padrão do projeto

--- SQL/insert_procedur.sql
+++ SQL/insert_procedur.sql
@@ -6,10 +6,10 @@ BEGIN
     -- Inserindo dados distintos na tabela RacaCorPaciente
     INSERT INTO RacaCorPaciente (CodigoRacaCorPaciente, NomeRacaCorPaciente)
     SELECT DISTINCT 
-           CAST(TRIM(co_raca_cor_paciente) AS INT),         -- Convers„o de NCHAR para INT 
-           CAST(TRIM(no_raca_cor_paciente) AS VARCHAR(50)) -- Convers„o de NVARCHAR para VARCHAR(50) 
+           CAST(TRIM(co_raca_cor_paciente) AS INT),         -- Convers√£o de NCHAR para INT 
+           CAST(TRIM(no_raca_cor_paciente) AS VARCHAR(50)) -- Convers√£o de NVARCHAR para VARCHAR(50) 
     FROM vacinacao_jan_2025;                         -- Tabela de origem importada via BULK INSERT
-    -- N„o precisa de WHERE pois n„o tem valores nulos
+    -- N√£o precisa de WHERE pois n√£o tem valores nulos
 
     PRINT 'Dados inseridos com sucesso na tabela RacaCorPaciente.';
 END;
